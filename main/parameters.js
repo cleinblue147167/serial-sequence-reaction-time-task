52690b24c0f8e9427f858bd6f4d3c49e8df08588
Daial <daianlu@wmu.edu.cn>
2025-11-18 16:34:00 +0800

    Push content from main folder to main branch

---
+++ main/parameters.js
@@ -0,0 +1,14 @@
+/*************** PARAMETERS OF THE TASK ***************/
+
+const language = zh; //available languages: english (en), hungarian (hu), french (fr), portuguese (pt), spanish (es), chinese (zh)
+const numberOfPracticeBlocks = 1;
+const numberOfBlocks = 20;
+const numberOfSequenceRepetitions = 10;
+const patternTrialImage = "url(../static/images/dalmata.jpg)";
+const randomTrialImage = "url(../static/images/dalmata.jpg)";
+const rsi = 500;
+const initialDelay = 1000;
+const overallWarning = false;
+const isOffline = false;
+const responseKeys = [['s', 'f', 'j', 'l']];
+const isFirstFiveRandom = false;
